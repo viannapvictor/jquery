604354e1f5b8fbff95cb1a5f7e9fe66c1047de00
Victor Vianna <vitorvianna27@gmail.com>
2023-01-28 21:06:09 -0300

    fim do módulo19

--- js/custom.js
+++ js/custom.js
@@ -11,11 +11,9 @@ $(document).ready(function(){
     
     let destaques = $('#featured') // id
 
-    console.log(titulos.first());
+    $('.featured-item a').addClass('btn btn-success stretch-link');
 
-    $('.featured-item a').addClass('btn btn-dark stretch-link');
-
-    $('.featured-item:first h4').append('<span class="badge bg-secondary">Novo</span>')
+    $('.featured-item:first h4').append('<span class="badge bg-s">Novo</span>')
 
     // $('.featured-item:first h4').start('<span class="badge bg-secondary">Novo</span>')
     // $('.featured-item:first h4').html('<span class="badge bg-secondary">Novo</span>')
@@ -27,24 +25,166 @@ $(document).ready(function(){
     // $('.featured-item:first h4').fadeIn(2000)
     // $('.featured-item:first h4').fadeOut()
     //  $('.featured-item:first h4').css('color', '#f00')
-    //$('.featured-item:first h4').css('color','#cfa')
+    //$('.featured-item:first h4').css('color','#cfa') 
+   //$('.featured-item a').on('blur', function(event){
+   //    event.preventDefault();
+   //   alert('Produto Esgotado')
+   // })
 
-    $('.featured-item h4').click(function(){
+    //CallBack
+   // $('.featured-item:nth(1)').hide(500, function(){
+   //     console.log( $(this).find('h4').text() + ' esgotado' )
+   // })
+   // .show(500, function(){
+   //    console.log( $(this).find('h4').text() + ' em estoque' )
+   //})
+    const duracao = 1000
+    $('.featured-item')
+    .hide(1)
+    .show(duracao)
+
+    $('#form-submit').on('click', function(e){
+
+        e.preventDefault()
+
+        if( $('#email').val() != '' ){
+
+            $('#email').animate({
+
+                opacity: "toggle",
+                top: "-50"
+            }, duracao, function(){
+                console.log($(this).val())
+            })
+        }
+           
+    })
+
+    /*
+    * Ouvinte de eventos 
+    */
+    $('.nav-modal-open').on('click', function(e){
+
+        e.preventDefault();
+  
+        let elem = $(this).attr('rel')
+  
+        $('.modal-body').html($('#'+elem).html())
         
-        $(this).css({
-            'color':'#cfa',
-            'background': '#ff0',
-            'font-wight': '100',
-        })
-    })
-    
-    $('.featured-item a').on('blur', function(event){
+        $('.modal-header h5.modal-title').html($(this).text())
+  
+        let myModal = new bootstrap.Modal($('#modelId'))
+  
+        myModal.show()
+  
+  
+     })
 
-        event.preventDefault();
+     function validate(elem){
+        if( elem.val() == '') {
+  
+           console.log('o campo de '+ elem.attr('name') + ' é obrigatório')
+  
+           elem.parent().find('.text-muted').show()
+  
+           elem.addClass('invalid')
+  
+           return false
+        } else {
+           elem.parent().find('.text-muted').hide()
+           elem.removeClass('invalid')
+        }
+     }
 
-        alert('Produto Esgotado')
+     function validateName(elem){
+        if(elem.val().length < 3) {
+  
+           console.log('o campo de '+ elem.attr('name') + ' é obrigatório')
+  
+           elem.parent().find('.text-muted').show()
+  
+           elem.addClass('invalid')
+  
+           return false
+        } else {
+           elem.parent().find('.text-muted').hide()
+           elem.removeClass('invalid')
+        }
+     }
 
-    })
+     function validateEmail(elem){
+        if(elem.val().includes('@') && elem.val().includes('.com')) {
+            elem.parent().find('.text-muted').hide()
+            elem.removeClass('invalid')
+
+        } else {
+              
+           console.log('o campo de '+ elem.attr('name') + ' é obrigatório')
+           elem.parent().find('.text-muted').show()
+           elem.addClass('invalid')
+           return false
+        }
+     }
+  
+     $('body').on('submit', '.modal-body .form', function(e){
+  
+        e.preventDefault()
+  
+        const inputName = $('#nome')
+        const inputEmail = $('#email')
+  
+        validateName(inputName)
+        validateEmail(inputEmail)
+  
+        if(inputName.hasClass('invalid') || inputEmail.hasClass('invalid')){
+           console.log('verificar campos obrigatórios')
+           return false
+        } else {
+           $(this).submit()  
+        }
+  
+     })
+
+     $('body').on('click', '#nome', function(){
+        validateName($(this))
+     })
+  
+     $('body').on('click', '#email', function(){
+        validateEmail($(this))
+     })
+
+     $('body').on('click', '#date', function(){
+        validate($(this))
+        $(this).mask('00/00/0000')
+     })
+
+     $('body').on('focus', '#date', function(){
+        $(this).datepicker()
+     })
+
+     $('body').on('click', '#time', function(){
+        validate($(this))
+        $(this).mask('00:00')
+     })
+
+     $('body').on('click', '#cep', function(){
+        validate($(this))
+        $(this).mask('00000-000')
+     })
+
+     $('body').on('click', '#phone', function(){
+        validate($(this))
+        $(this).mask('00000-0000')
+     })
+
+     $('body').on('click', '#cpf', function(){
+        validate($(this))
+        $(this).mask('000.000.000-00')
+        // desenvoli toda a regex, mas não consegui aplicar nas mascara
+        //"/^\d{3}.?\d{3}.?\d{3}-?\d{2}$/"
+        // alguma ideia?
+     })
+
 
 })
 
